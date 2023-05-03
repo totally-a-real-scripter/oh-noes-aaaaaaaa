49745ba6107cdd56087c189ce2a345beaffab67d
totally-a-real-scripter <85376995+totally-a-real-scripter@users.noreply.github.com>
2023-05-02 21:10:38 -0500

    Update um wtf.js

--- um wtf.js
+++ um wtf.js
@@ -1,3 +1,3 @@
-//* i forgor how to javascript *//
+// forgor how to javascript anyway copy that and then copy the other thing in it idk i dont care EASTER EGG balls
 
 javascript:(()=>{let a=prompt("go here: https://raw.githubusercontent.com/totally-a-real-scripter/oh-noes-aaaaaaaa/main/tc");;eval(`(()=>{${a}})()`)})()
